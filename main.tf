214250e1ddabc7f0869f92e955079f33ac975d03
Stephen Somuah <stephen.somuah@bigspark.dev>
2026-01-20 19:32:46 +0000

    added include global resource types

--- main.tf
+++ main.tf
@@ -121,7 +121,7 @@ resource "aws_config_configuration_recorder" "config_recorder" {
 
   recording_group {
     all_supported              = true
-    include_global_resource_types = true
+    include_global_resource_types = false
   }
 }
 
